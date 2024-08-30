70c46284ad372e8116635f1cfddf27cb3c60dcfc
amisalaam <amisalaam9@gmail.com>
2024-08-30 19:27:03 +0530

    User Registeration completed

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 
-# Create your models here.
+class UserManager(BaseUserManager):
+    def create_user(self, email, fullName, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, fullName=fullName, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, fullName, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, fullName, password, **extra_fields)
+
+class User(AbstractBaseUser, PermissionsMixin):
+    fullName = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    
+    username = None
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['fullName']
+    
+    objects = UserManager()
+    
+    def __str__(self):
+        return self.email
--- authentication/urls.py
+++ authentication/urls.py
@@ -1,2 +1,8 @@
 from django.urls import path
+from .views import CreateUserView
 
+urlpatterns = [
+    
+    path('api/register/', CreateUserView.as_view(), name='register'),
+    
+]
